915c35e5e87e888bada6417a20eed03de7c17d64
SKorchinskiy <ddphyk@gmail.com>
2023-07-15 17:30:40 +0300

    + add auth service

---
+++ src/services/auth.service.js
@@ -0,0 +1,45 @@
+const bcrypt = require("bcrypt");
+
+async function signUp(user) {
+  const { email } = user;
+  if (await getUserByEmail(email)) {
+    throw new Error({
+      status: 400,
+      message: `The email ${email} is already in use! Please, try another email`,
+    });
+  }
+  // create new user and store it into db
+}
+
+async function signIn(credentials) {
+  const { email, password } = credentials;
+  const user = await getUserByEmail(email);
+  if (!user) {
+    throw new Error({
+      status: 400,
+      message: `Invalid credentials! Email or password doesn't match`,
+    });
+  }
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) {
+    throw new Error({
+      status: 400,
+      message: `Invalid credentials! Email or password doesn't match`,
+    });
+  }
+  // assign cookies to user
+}
+
+async function signOut() {
+  // remove user's auth cookies
+}
+
+async function getUserByEmail(email) {
+  return {};
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  signOut,
+};
