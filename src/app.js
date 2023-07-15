8242933b9547346084bbc177ff94aacedc9064de
SKorchinskiy <ddphyk@gmail.com>
2023-07-15 21:12:38 +0300

    + add api routes to express app

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const express = require("express");
 
+const router = require("./routes");
+
 const app = express();
 
 app.use(helmet());
 app.use(morgan("dev"));
+
+app.use("/api", router);
 
 app.all("*", (req, res) => {
   return res.status(404).json({
