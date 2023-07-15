533b08a0726dbf81d2e7687f598b6209e3b01c62
SKorchinskiy <ddphyk@gmail.com>
2023-07-15 21:10:59 +0300

    + add single source of user routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+
 const authRouter = require("./auth.router");
+const userRouter = require("./user.router");
 
 const router = express.Router();
 
-router.use("/auth", authRouter);
+router.use("/auth", authRouter).use("/user", userRouter);
 
 module.exports = router;
