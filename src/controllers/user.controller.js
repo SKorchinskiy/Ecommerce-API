b6062d2b81d8fa4fab7f71618271cd7fbe7a7fad
SKorchinskiy <ddphyk@gmail.com>
2023-07-15 19:47:02 +0300

    + add user controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,105 @@
+const userService = require("../services/user.service");
+
+async function createUser(req, res) {
+  try {
+    const userData = req.body;
+    return await userService.createUser(userData);
+  } catch (error) {
+    const { status, message } = error;
+    return res.status(status).json({
+      success: false,
+      message,
+    });
+  }
+}
+
+async function getUserById(req, res) {
+  try {
+    const { id } = req.body;
+    return await userService.getUserById(id);
+  } catch (error) {
+    const { status, message } = error;
+    return res.status(status).json({
+      success: false,
+      message,
+    });
+  }
+}
+
+async function getUserByUsername(req, res) {
+  try {
+    const { username } = req.body;
+    return await userService.getUserByUsername(username);
+  } catch (error) {
+    const { status, message } = error;
+    return res.status(status).json({
+      success: false,
+      message,
+    });
+  }
+}
+
+async function getUserByEmail(req, res) {
+  try {
+    const { email } = req.body;
+    return await userService.getUserByEmail(email);
+  } catch (error) {
+    const { status, message } = error;
+    return res.status(status).json({
+      success: false,
+      message,
+    });
+  }
+}
+
+async function updateUserById(req, res) {
+  try {
+    const { id, ...data } = req.body;
+    return await userService.updateUserById(id, data);
+  } catch (error) {
+    const { status, message } = error;
+    return res.status(status).json({
+      success: false,
+      message,
+    });
+  }
+}
+
+async function deleteUserById(req, res) {
+  try {
+    const { id } = req.body;
+    return await userService.deleteUserById(id);
+  } catch (error) {
+    const { status, message } = error;
+    return res.status(status).json({
+      success: false,
+      message,
+    });
+  }
+}
+
+async function resetPasswordByEmail(req, res) {
+  try {
+    const { email } = req.body;
+    return await userService.resetPasswordByEmail(email);
+  } catch (error) {
+    const { status, message } = error;
+    return res.status(status).json({
+      success: false,
+      message,
+    });
+  }
+}
+
+async function updatePassword(req, res) {
+  try {
+    const credentials = req.body;
+    return await userService.updatePassword(credentials);
+  } catch (error) {
+    const { status, message } = error;
+    return res.status(status).json({
+      success: false,
+      message,
+    });
+  }
+}
