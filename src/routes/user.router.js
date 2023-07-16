df1fed5a88a317e6f29413a51bf41a07f549b167
SKorchinskiy <ddphyk@gmail.com>
2023-07-16 23:20:59 +0300

    + add authentication for using user controller

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const userController = require("../controllers/user.controller");
+const { isAuthenticated } = require("../middlewares/auth.middleware");
 
 const userRouter = express.Router();
+
+userRouter.use(isAuthenticated);
 
 userRouter
   .route("/")
