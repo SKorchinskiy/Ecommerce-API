a10734a1790c4e5ba0fa5a4958fa3aec36eea37d
SKorchinskiy <ddphyk@gmail.com>
2023-07-16 23:20:34 +0300

    + add authorization for auth log out route

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authController = require("../controllers/auth.controller");
+const { isAuthenticated } = require("../middlewares/auth.middleware");
 
 const authRouter = express.Router();
 
@@ -7,7 +8,7 @@ authRouter.route("/sign-in").post(authController.signIn);
 
 authRouter.route("/sign-up").post(authController.signUp);
 
-authRouter.route("/sign-out").post(authController.signOut);
+authRouter.route("/sign-out").all(isAuthenticated).post(authController.signOut);
 
 authRouter.route("/forgot").post(authController.resetPassword);
 
