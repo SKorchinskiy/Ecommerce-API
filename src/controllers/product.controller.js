3eb64c92c405cc8584840d81f126171645f0e7a4
SKorchinskiy <ddphyk@gmail.com>
2023-07-17 22:39:16 +0300

    + add product controller ties with service

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,9 +1,77 @@
-const db = require("../configs/db.config");
+const productService = require("../services/product.service");
 
-async function getProductById(req, res) {}
+async function getProductById(req, res) {
+  try {
+    const { id } = req.params;
+    const product = await productService.getProductById(id);
+    return res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    const { status, message } = error;
+    return res.status(status || 500).json({
+      success: false,
+      message,
+    });
+  }
+}
 
-async function createProduct(req, res) {}
+async function createProduct(req, res) {
+  try {
+    const data = req.body;
+    const product = await productService.createProduct(data);
+    return res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    const { status, message } = error;
+    return res.status(status || 500).json({
+      success: false,
+      message,
+    });
+  }
+}
 
-async function updateProductById(req, res) {}
+async function updateProductById(req, res) {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const product = await productService.updateProductById(id, data);
+    return res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    const { status, message } = error;
+    return res.status(status || 500).json({
+      success: false,
+      message,
+    });
+  }
+}
 
-async function deleteProductById(req, res) {}
+async function deleteProductById(req, res) {
+  try {
+    const { id } = req.params;
+    const product = await productService.deleteProductById(id);
+    return res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    const { status, message } = error;
+    return res.status(status || 500).json({
+      success: false,
+      message,
+    });
+  }
+}
+
+module.exports = {
+  getProductById,
+  createProduct,
+  updateProductById,
+  deleteProductById,
+};
