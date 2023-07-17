7ed373bbe72d4642e63f6ef2b966a44bc5e3ef26
SKorchinskiy <ddphyk@gmail.com>
2023-07-17 23:21:51 +0300

    + add auth for product routes

--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const productController = require("../controllers/product.controller");
+const { isAuthenticated } = require("../middlewares/auth.middleware");
+const { isGrantedAccess } = require("../middlewares/role.middleware");
 
 const productRouter = express.Router();
+
+const adminHandlers = [isAuthenticated(), isGrantedAccess("admin")];
+
+productRouter
+  .route("/")
+  .get(productController.getAllProducts)
+  .post(...adminHandlers, productController.createProduct);
 
 productRouter
   .route("/:id")
   .get(productController.getProductById)
-  .post(productController.createProduct)
-  .put(productController.updateProductById)
-  .delete(productController.deleteProductById);
+  .put(...adminHandlers, productController.updateProductById)
+  .delete(...adminHandlers, productController.deleteProductById);
 
 module.exports = productRouter;
