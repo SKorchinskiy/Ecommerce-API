7318ff9fe6048a58fb316ba4c560dba184f7ace5
SKorchinskiy <ddphyk@gmail.com>
2023-07-17 22:03:38 +0300

    + add basic structure for product controller

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,9 @@
+const db = require("../configs/db.config");
+
+async function getProductById(req, res) {}
+
+async function createProduct(req, res) {}
+
+async function updateProductById(req, res) {}
+
+async function deleteProductById(req, res) {}
