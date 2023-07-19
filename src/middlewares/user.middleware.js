065d8d0febabc2d4bf6c1879e39ad6dc457e8d14
SKorchinskiy <ddphyk@gmail.com>
2023-07-19 19:56:34 +0300

    + update joi import name

--- src/middlewares/user.middleware.js
+++ src/middlewares/user.middleware.js
@@ -1,11 +1,11 @@
-const joi = require("joi");
+const Joi = require("joi");
 const userValidationSchema = require("../schemas/user.schema");
 
 function validateUserInput() {
   return (req, res, next) => {
     try {
       const data = req.body;
-      joi.assert(data, userValidationSchema);
+      Joi.assert(data, userValidationSchema);
       next();
     } catch (error) {
       const message = error?.details[0]?.message;
