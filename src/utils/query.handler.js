5d3c0888b6b711d6060d846724ce86272b239077
SKorchinskiy <ddphyk@gmail.com>
2023-07-26 17:44:37 +0300

    + update query pagination

--- src/utils/query.handler.js
+++ src/utils/query.handler.js
@@ -4,8 +4,8 @@ const DEFAULT_LIMIT_NUMBER = 10;
 function getPagination(query) {
   const page = +query.page || DEFAULT_PAGE_NUMBER;
   const limit = +query.limit || DEFAULT_LIMIT_NUMBER;
-  const skip = (page - 1) * limit;
-  return { skip, limit };
+  const offset = (page - 1) * limit;
+  return { offset, limit };
 }
 
 module.exports = { getPagination };
