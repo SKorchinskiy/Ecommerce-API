61326efcada72a4405afc5d412f265059fbf7466
SKorchinskiy <ddphyk@gmail.com>
2023-07-27 18:08:49 +0300

    + setup redis storage

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 const http = require("http");
 
 const app = require("./app");
+const { startRedis } = require("./configs/storage.config");
 
 require("dotenv").config();
 
 const httpServer = http.createServer(app);
 
 async function startServer() {
+  await startRedis();
+
   httpServer.listen(process.env.PORT, () => {
     console.log(`The application is listening on port ${process.env.PORT}`);
   });
