12edca0cdefa2ccca9ceae40eec90dabcf7371d8
SKorchinskiy <ddphyk@gmail.com>
2023-07-28 17:31:48 +0300

    + restructure user router

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -3,27 +3,31 @@ const userController = require("../controllers/user.controller");
 const { isAuthenticated } = require("../middlewares/auth.middleware");
 const { isGrantedAccess } = require("../middlewares/role.middleware");
 const { validateUserInput } = require("../middlewares/user.middleware");
+const { isResourceOwnerOrAdmin } = require("../middlewares/owner.middleware");
 
 const userRouter = express.Router();
 
-userRouter.use(
-  isAuthenticated(),
-  isGrantedAccess("admin"),
-  validateUserInput()
-);
+userRouter.use(isAuthenticated());
 
 userRouter
   .route("/")
-  .get(userController.getAllUsers)
-  .post(userController.createUser);
+  .get(isGrantedAccess("admin"), userController.getAllUsers)
+  .post(validateUserInput(), userController.createUser);
 
 userRouter
   .route("/:id")
   .get(userController.getUserById)
-  .put(userController.updateUserById)
-  .delete(userController.deleteUserById);
+  .put(
+    isResourceOwnerOrAdmin(),
+    validateUserInput(),
+    userController.updateUserById
+  )
+  .delete(isResourceOwnerOrAdmin(), userController.deleteUserById);
 
-userRouter.route("/email/:email").get(userController.getUserByEmail);
+userRouter
+  .route("/email/:email")
+  .all(isGrantedAccess("admin"))
+  .get(userController.getUserByEmail);
 
 userRouter.route("/username/:username").get(userController.getUserByUsername);
 
